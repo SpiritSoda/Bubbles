2d20e2f14eb5e3837fec354cb90d7dd10f2974b2
SpiritSoda <1800013119@pku.edu.cn>
2023-04-17 15:15:43 +0800

    [Document Added]

--- Bubbles_frontend/vite.config.js
+++ Bubbles_frontend/vite.config.js
@@ -6,22 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.ELECTRON=="true" ? './' : "./",
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output:{
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-                return id.toString().split('node_modules/')[1].split('/')[0].toString();
-            }
-        }
-      }
-  }
   }
 })
